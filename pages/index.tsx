4eae3c13466befc52321ffb03c291f34f51b1324
YawerAli <yawer.ali@thewitslab.com>
2023-01-17 18:46:15 +0530

    feat: routing

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,15 +28,32 @@ export default function Home() {
         News Application
       </h1>
       <div>
-        <Link href="/users">
-          <h6>Users</h6>
-        </Link>
-        <Link href="/users/about">
-          <h6>About Us</h6>
-        </Link>
-        <Link href={`/users/${id}`}>
-          <h6>User With Id {id}</h6>
-        </Link>
+        <ul className="flex border-b">
+          <li className="-mb-px mr-1">
+            <Link
+              href="/users"
+              className="bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-blue-700 font-semibold"
+            >
+              <h6>Users</h6>
+            </Link>
+          </li>
+          <li className="mr-1">
+            <Link
+              href="/users/about"
+              className="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold"
+            >
+              <h6>About Us</h6>
+            </Link>
+          </li>
+          <li className="mr-1">
+            <Link
+              href={`/users/${id}`}
+              className="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold"
+            >
+              <h6>User With Id {id}</h6>
+            </Link>
+          </li>
+        </ul>
       </div>
       {posts?.map((post: any) => (
         <Post key={post.id} post={post} />
